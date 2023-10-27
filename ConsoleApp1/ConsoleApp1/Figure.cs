1c30822da77d2b63ae2c47c1ae65d6f37a5bf23c
salimovrashad <selimovresad33@gmail.com>
2023-10-27 11:22:28 +0400

    add task

---
+++ ConsoleApp1/ConsoleApp1/Figure.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	public abstract class Figure
+	{
+		public abstract int CalcArea();
+	}
+
+	public class Square : Figure
+	{
+		public int side;
+
+		public Square(int x) 
+		{ 
+			side = x;
+		}
+		public override int CalcArea() 
+		{
+            Console.WriteLine("Sahe: ");
+			return (side*side);
+        }
+	}
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("eded daxil et: ");
+			int num = Convert.ToInt32(Console.ReadLine());
+            Figure square = new Square(num);
+            Console.WriteLine(square.CalcArea());
+        }
+	}
+}
